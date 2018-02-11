de8c3adf9475f5c1978b908c426f487f0bc8c7b3
hurdles <mahantheshck012@gmail.com>
2018-02-11 15:07:02 +0530

    First commit

---
+++ mouse.py
@@ -0,0 +1,28 @@
+class Pointer(object):
+
+
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+
+
+	def moveup(self):
+		self.y=self.y + 1
+		#self.y +=1
+
+
+	def movedown(self):
+		self.y=self.y-1
+		#self.y -=1
+
+
+	def moveleft(self):
+		self.x -=1
+
+	def moveright(self):
+		self.x +=1
+
+	def position(self):
+		x=self.x
+		y=self.y
+		print(self.x,self.y)
